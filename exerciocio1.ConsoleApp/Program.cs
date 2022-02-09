ceb1322956315be620d9a775c0aa05cc43a25639
nicol <nicol@DESKTOP-0809H1K>
2022-02-09 17:09:35 -0300

    Add project files.

---
+++ exerciocio1.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exerciocio1.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Terrenos Retangulares!");
+            Console.WriteLine("Qual a altura do Terreno");
+            float altura = float.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a largura do terreno ?");
+            float largura = float.Parse(Console.ReadLine());
+            float area = altura * largura;
+            Console.WriteLine($" A area do seu terreno é de {area}");
+            Console.WriteLine();
+        }
+    }
+}
